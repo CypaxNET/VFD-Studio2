dca31d63e0dea11e1feab543d92476a40c3c0d9c
Cypax <github@cypax.net>
2024-02-07 16:56:05 +0100

    List file extension now *.vfdlst; Code beautified; some methods moved to dedicate file

--- VFD-Studio2/StudioCommon.pas
+++ VFD-Studio2/StudioCommon.pas
@@ -6,7 +6,6 @@ interface
 
 uses
   Classes, SysUtils, Graphics;
-
 type
   TLogLevel = (lvINFO, lvWARNING, lvERROR, lvCRITICAL);
 
@@ -17,16 +16,16 @@ type
   TSearchDirection = (sdForward, sdBackward);
 
   TApplicationConfig = record
-    Language: string;
+    Language: String;
     PreviewDisplayColor: TColor; // color of the pixels in the preview display
     DoStartMinimized: Boolean;   // start VFD-Studio2 minimized in system tray?
   end;
 
   TDisplayConfig = record
-    DisplayType: string; // e.g. 'NTK800'
+    DisplayType: String; // e.g. 'NTK800'
     ResX: Word;          // display resolution in x direction
     ResY: Word;          // display resolution in y direction
-    IntName: string;     // interface name (e.g. 'COM5')
+    IntName: String;     // interface name (e.g. 'COM5')
     Baudrate: Cardinal;  // baudrate for serial display connection
     IsBrightnessControlledByList: Boolean; // display brightness is controlled by list commands (otherwise by following setting)
     DisplayBrightness: Byte; // display brightness in percent, unless brightness is controlled by list commands
@@ -34,7 +33,7 @@ type
   end;
 
   TListConfig = record
-    ListName: string;
+    ListName: String;
   end;
 
   TAnimationConfig = record
@@ -50,10 +49,10 @@ type
   end;
 
   TVariableInfo = record
-    Text: string;
-    SubsText: string;    // substituted content of the string; used to determine if it needs to be re-drawn
+    Text: String;
+    SubsText: String;    // substituted content of the string; used to determine if it needs to be re-drawn
     X, Y: Byte;
-    FontName: string;
+    FontName: String;
     FontSize: Integer;
     PrevWidth: Integer;  // width of the string before we updated it
     PrevHeight: Integer; // height of the string before we updated it; this is only relevant when handling text with a font
@@ -69,7 +68,7 @@ type
 
   // structure of one column of text dropping down the display
   TMatrixDrop = record
-    Text: string;          // goes bottom up: first char will be in lowest column
+    Text: String;          // goes bottom up: first char will be in lowest column
     MaxTextLen: Byte;      // maximum length of this drop
     Row: Integer;          // current row (where the first character is located); can be out of display area
     SlownessFactor: Byte;  // controls the speed of the drop; the higher, the slower
@@ -111,7 +110,11 @@ type
     IsUsageMonitorDisplayed: Boolean; // is the memory usage monitor currently displayed?
   end;
 
+const
+  STUDIO_COMMANDS = 'NEWSCREEN SCREENEND CLEARSCREEN SCREENTIME STOP ORMODE XORMODE ANDMODE LIGHT PLAINTEXT TEXTOUT BITMAP ANIMATE PIXEL LINE FRAME NOISE CLOCK DRIVEUSAGE RAMMONITOR CPUMONITOR THEMATRIX';
+  STUDIO_KEYWORDS_STATIC = 'REGKEY_ SLOTUSAGE SLOTTYPE SLOTINFO VERSION PCNAME USERNAME OS OSNAME CPUCORES CPUMAX CPUVENDOR CPUSOCKET CPUNAME CPUIDENT CPUFAMILY TIMEZONE RECENTDD RECENTD3D BIOSVERSION BIOSVENDOR BIOSDATE BOARDVENDOR BOARDPRODUCT BOARDVERSION';
+  STUDIO_KEYWORDS_VARIABLE = 'TOTALDRIVE FREEDRIVE NAMEDRIVE MEMORY FREEMEM MEMUSAGE AVERAGECPU CPUUSAGE IP UPTIME CPUSPEED RES COLORS FREQ WAVERSION WATITLE WALENGTH WAPOS DOW DATE TIME YEAR MON DAY HH MM SS :';
+
 implementation
 
 end.
-
